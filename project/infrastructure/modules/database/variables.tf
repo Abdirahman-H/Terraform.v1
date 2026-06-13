104c31c95340abaece9b083d1fe2b21d39d587dc
Your Name <you@example.com>
2026-06-13 12:08:14 +0100

    fmt files

--- project/infrastructure/modules/database/variables.tf
+++ project/infrastructure/modules/database/variables.tf
@@ -7,7 +7,7 @@ variable "db_username" {
   description = "The username for the database"
   type        = string
   sensitive   = true
-  
+
 }
 
 variable "db_password" {
--- project/infrastructure/modules/networking/main.tf
+++ project/infrastructure/modules/networking/main.tf
@@ -7,8 +7,8 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "subnets" {
   for_each = var.subnets
 
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = each.value.cidr_block
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = each.value.cidr_block
   availability_zone = each.value.availability_zone
 }
 
