5af8364a8609bf8c22aeec05f2f6ccc78570e72c
Your Name <you@example.com>
2026-06-13 12:11:11 +0100

    fmt main.tf

--- project/backend/main.tf
+++ project/backend/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bkend_ah_state" {
-  bucket = "backend-ah-state"
-  force_destroy = true 
+  bucket        = "backend-ah-state"
+  force_destroy = true
 
   tags = {
     Name        = "backend"
